fe8754f9290d603d17000b11d23b493dd6b3a882
none <https://github.com/vf12776-ux>
2026-01-03 20:12:45 +0300

    Fix manifest and service worker from working project

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,32 @@
-const CACHE_NAME = 'kirakan-reader-cache';
+const CACHE_VERSION = 'v1';
+const CACHE_NAME = `kirakan-cache-${CACHE_VERSION}`;
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon192.png',
+  '/icons/icon512.png'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => 
-      cache.addAll(['/', '/index.html', '/manifest.json', '/offline.html'])
-    )
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
@@ -12,6 +34,5 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
-      .catch(() => caches.match('/offline.html'))
   );
 });
